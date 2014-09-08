c57fdb9b02671945123d0db4fda78237e31c19e4
Jessica Owens <jessicadanaowens@gmail.com>
2014-09-08 12:58:22 -0600

    fixed dates and patient id in form

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -5,18 +5,32 @@ class PrescriptionsController < ApplicationController
   end
 
   def create
+    puts "*" * 80
+    puts params
 
     def med_id
       params[:prescription][:medication_id]
     end
+
+    start_date = Date.new(
+      params[:prescription]["starts_on(1i)"].to_i,
+      params[:prescription]["starts_on(2i)"].to_i,
+      params[:prescription]["starts_on(3i)"].to_i,
+    )
+
+    end_date = Date.new(
+      params[:prescription]["ends_on(1i)"].to_i,
+      params[:prescription]["ends_on(2i)"].to_i,
+      params[:prescription]["ends_on(3i)"].to_i,
+    )
 
     prescription = Prescription.new(
       :medication_id => med_id,
       :patient_id => params[:patient_id],
       :dosage => params[:prescription][:dosage],
       :schedule => params[:prescription][:schedule],
-      :starts_on => params[:prescription][:starts_on],
-      :ends_on => params[:prescription][:ends_on],
+      :starts_on => start_date,
+      :ends_on => end_date,
       :user_id => session[:user_id]
     )
 
